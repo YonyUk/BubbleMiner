7d4f1d4990f693da55882dee7a33632cec0009fb
cuervo <alex.cuervogrillo@gmail.com>
2025-01-24 15:13:53 -0500

    alala

---
+++ Scripts/GlobalVariables.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Variables{
+
+    public static class Globals{
+        public static string playerTag = "Player";
+    }
+
+}
