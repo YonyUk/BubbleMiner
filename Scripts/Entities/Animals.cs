53365c01d8786c7e5994ad4af8a800df7b188461
Kikiale <kikialeglez@gmail.com>
2025-01-24 14:19:55 -0500

    Animal con la agresividad creada y colider tambien

--- Scripts/Entities/Animals.cs
+++ Scripts/Entities/Animals.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
+using Architecture.Eqquipables;
+using Architecture.Resource;
 
 public class Animals : MonoBehaviour
 {
     public int Damage;
     public int Aggressive;
-    public GameObject food;
-    public GameObject Bait; //Tiene que ser un IEquipable
+    public IResource Food;
+    public IEqquipable Bait;
+    public bool Flag = false;//Bandera para indicar al game controler que el animal va al ataque
 
-    public Animals(int Damage,int Aggressive, GameObject Bait){
-        this.Damage = Damage;
-        this.Aggressive = Aggressive;
-        this.Bait = Bait;
-    }
-    public bool Attack(int regulator = 11){
+    private bool Attack(IEqquipable bait = null){
         //Falta el aumento de agresividad del bait
+        int regulator = 11;
+        if(bait != null && Atracted(bait)){
+            Aggressive += 5;
+            regulator = 16;
+        }
         if(Aggressive%regulator > Random.Range(1,10)){
             return true;
         }
         return false;
     }
-    public GameObject Drop(){
-
+    private bool Atracted (IEqquipable bait){
+        if(bait.Name == Bait.Name){
+            return true;
+        }
+        return false;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        IEqquipable bait = other.gameObject.GetComponent<IEqquipable>();
+        if(Attack(bait)){
+            Flag = true;
+        }
     }
 }
