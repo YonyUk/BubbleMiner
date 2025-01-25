9df0288f1bcf6a4ff4ddff1a1bd9663f869244a3
YonyUk <yonatanjoseguerraperez@gmail.com>
2025-01-26 00:05:22 +0100

    saving to switch branches

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -9,28 +9,28 @@ public class NewBehaviourScript : MonoBehaviour
     private void Move(){
         float inputHorizontal = Input.GetAxis("Horizontal");
         float inputVertical = Input.GetAxis("Vertical");
-        Vector3 direction = new(inputHorizontal, 0, inputVertical);
+        //Vector3 direction = new(inputHorizontal, 0, inputVertical);
 
-        Matrix4x4 matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
-        Vector3 fixedDirection = matrix.MultiplyVector(direction);  
-        direction.Normalize();
-        GetComponent<CharacterController>().Move(fixedDirection * 4 * Time.deltaTime);
+        //Matrix4x4 matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
+        //Vector3 fixedDirection = matrix.MultiplyVector(direction);  
+        //direction.Normalize();
+        //GetComponent<CharacterController>().Move(fixedDirection * 4 * Time.deltaTime);
 
     }
     
     private void LookAtCursor(){
         var mousePos = mainCamera.ScreenPointToRay(Input.mousePosition);
         Vector3 direction;
-        if(Physics.Raycast(mousePos, out var hitInfo, Mathf.Infinity, 1)){
-            if(hitInfo.transform.CompareTag(Globals.playerTag)) direction = new(1,0,1);
-            else direction = hitInfo.point - transform.position;
-            direction.y = 0;
-        }
-        else {
-            direction = Vector3.forward;
-        }
+        //if(Physics.Raycast(mousePos, out var hitInfo, Mathf.Infinity, 1)){
+        //    if(hitInfo.transform.CompareTag(Globals.playerTag)) direction = new(1,0,1);
+        //    else direction = hitInfo.point - transform.position;
+        //    direction.y = 0;
+        //}
+        //else {
+        //    direction = Vector3.forward;
+        //}
 
-        transform.forward = direction;
+        //transform.forward = direction;
     }
      
     void Start()
