5890faf9801d362aeee7079ca7f2f1979b246938
YonyUk <yonatanjoseguerraperez@gmail.com>
2025-01-26 09:29:09 +0100

    generator map

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Generators;
+
+public class Main : MonoBehaviour {
+
+	Generator generator;
+	public int rows;
+	public int cols;
+	public int iterations;
+	public int types;
+	public float probability;
+	// Use this for initialization
+	void Start () {
+		generator = new Generator (rows, cols, types, iterations,probability);
+		generator.Generate ();
+		PrintMap ();
+	}
+	void PrintMap(){
+		for (int i = 0; i < rows; i++) {
+			string row = "";
+			for (int j = 0; j < cols; j++)
+				row += generator.Map[i,j] + " ";
+			Debug.Log(row);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
